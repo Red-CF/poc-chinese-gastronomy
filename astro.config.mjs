3a65dfa60aa4e4b090928f55c49b5846eb21fafc
carlos-rojas <carlos.rojasf@alten.es>
2026-03-31 20:48:19 +0200

    Fix base path typo for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.netlify.app/configuration/
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  base: '/poc-chinesse-gastronomy'
+  base: '/poc-chinese-gastronomy'
 });
